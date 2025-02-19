9c801c4791a6486fd585ba86c7fa7b4cd034fa7c
macman1717 <mchawk1717@gmail.com>
2025-02-19 01:29:06 -0500

    added code for week 6

---
+++ Week_6_Interfaces/CollegeStudent.java
@@ -0,0 +1,59 @@
+package Week_6_Interfaces;
+
+public class CollegeStudent{
+    private int stuID;
+    private String name;
+    private String major;
+    private int ageInYears;
+
+    public CollegeStudent(int stuID, String name, String major, int ageInYears) {
+        this.stuID = stuID;
+        this.name = name;
+        this.major = major;
+        this.ageInYears = ageInYears;
+    }
+
+    public int getStuID() {
+        return stuID;
+    }
+
+    public void setStuID(int stuID) {
+        this.stuID = stuID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public int getAgeInYears() {
+        return ageInYears;
+    }
+
+    public void setAgeInYears(int ageInYears) {
+        this.ageInYears = ageInYears;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("FinalReview.CollegeStudent{");
+        sb.append("stuID=").append(stuID);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", major='").append(major).append('\'');
+        sb.append(", ageInYears=").append(ageInYears);
+        sb.append('}');
+        return sb.toString();
+    }
+
+}
