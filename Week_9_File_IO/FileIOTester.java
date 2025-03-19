cd83130854f8a72825d6ec5a2f36c410cb5c7af4
macman1717 <mchawk1717@gmail.com>
2025-03-19 02:40:10 -0400

    week 9 starter

--- Week_9_File_IO/FileIOTester.java
+++ Week_9_File_IO/FileIOTester.java
@@ -1,48 +1,7 @@
 package Week_9_File_IO;
 
-import java.io.*;
-import java.util.*;
-
 public class FileIOTester {
     public static void main(String[] args) {
-        ArrayList<String> lines = new ArrayList<>();
 
-        File file = new File("src/IO/spongebob.txt");
-        File file2 = new File("src/IO/file2.txt");
-
-        try {
-            Scanner scanner = new Scanner(file);
-
-            FileWriter file2Writer = new FileWriter(file2);
-            BufferedWriter bufferedWriter = new BufferedWriter(file2Writer);
-
-            while(scanner.hasNext()){
-                lines.add(scanner.nextLine());
-            }
-
-            for(int i = lines.size() - 1; i >= 0; i--){
-                bufferedWriter.write(lines.get(i));
-                bufferedWriter.newLine();
-            }
-
-            bufferedWriter.close();
-            scanner.close();
-        }catch(IOException ex){
-            System.out.println(ex.getMessage());
-        }
-
-        for(String line : lines) {
-            System.out.println(line);
-        }
-
-        recursion(5);
-
-    }
-
-
-    public static void recursion(int x){
-        if(x == 0) return;
-        System.out.println("Hello");
-        recursion( x - 1);
     }
 }
