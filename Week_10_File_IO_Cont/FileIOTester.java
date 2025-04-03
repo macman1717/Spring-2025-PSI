0eca503e1a8a9d5c07f9f61e332c7791c58a1040
macman1717 <mchawk1717@gmail.com>
2025-04-03 10:41:08 -0400

    finished week 10

--- Week_10_File_IO_Cont/FileIOTester.java
+++ Week_10_File_IO_Cont/FileIOTester.java
@@ -2,15 +2,16 @@ package Week_10_File_IO_Cont;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class FileIOTester {
     public static void main(String[] args) {
         ArrayList<Student> students = new ArrayList<Student>();
-        students.add(new Student(900123456, "Connor Griffin", "Software Development", 148));
-        students.add(new Student(900111111, "Billy Bob", "Business Management", 74));
-        students.add(new Student(900222222, "Elijah Wood", "Data Science", 120));
-        students.add(new Student(900333333, "Brad Pitt", "Film", 43));
+        students.add(new Student(900123456, "Connor Griffin", "Software Development", 148, new String[]{"Soft-Dev 2","Web Development","Artificial Intelligence","Advanced Databases"}));
+        students.add(new Student(900111111, "Billy Bob", "Business Management", 74, new String[]{"Intermediate Programming","Intro to Databases","Tennis"}));
+        students.add(new Student(900222222, "Elijah Wood", "Data Science", 120, new String[]{"Pre-calc","Intro to Programming"}));
+        students.add(new Student(900333333, "Brad Pitt", "Film", 43, new String[]{"Biology", "Badminton", "Algebra"}));
 
         File studentFile = new File("Week_10_File_IO_Cont/students.txt");
         try(
@@ -19,10 +20,18 @@ public class FileIOTester {
                 ){
             for (Student student : students) {
                 String line = "";
-                line += student.getStuID() + ",";
-                line += student.getName() + ",";
-                line += student.getMajor() + ",";
+                line += student.getStuID() + "|";
+                line += student.getName() + "|";
+                line += student.getMajor() + "|";
                 line += student.getEarnedCredits();
+                line += "|";
+                for(int i = 0; i < student.getCourses().length; i++) {
+                    if (i == student.getCourses().length - 1) {
+                        line += student.getCourses()[i];
+                    }else{
+                        line += student.getCourses()[i] + ",";
+                    }
+                }
                 System.out.println(line);
                 bufferedWriter.write(line);
                 if(student != students.getLast())
@@ -38,12 +47,14 @@ public class FileIOTester {
                 ) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                String[] parts = line.split(",");
+                String[] parts = line.split("\\|");
                 long stuID = Long.parseLong(parts[0]);
                 String name = parts[1];
                 String major = parts[2];
                 int credits = Integer.parseInt(parts[3]);
-                studentsReadFromFile.add(new Student(stuID, name, major, credits));
+                String[] courses = parts[4].split(",");
+
+                studentsReadFromFile.add(new Student(stuID, name, major, credits, courses));
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
