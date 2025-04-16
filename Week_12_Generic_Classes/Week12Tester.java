d7308fa3fbf9d49f25477c18421a6b74d44d8af1
macman1717 <mchawk1717@gmail.com>
2025-04-16 14:54:32 -0400

    week12tester

---
+++ Week_12_Generic_Classes/Week12Tester.java
@@ -0,0 +1,16 @@
+package Week_12_Generic_Classes;
+
+import Week_11_Generics.Animals.*;
+
+import java.util.ArrayList;
+
+public class Week12Tester {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Fiona", 4, "Black", true);
+        Panda panda = new Panda("Po", 16, "White and Black");
+        RedPanda redPanda = new RedPanda("Priya", 2, "Red and White");
+        Frog frog = new Frog("Bob",5,"green");
+        Salamander salamander = new Salamander("Alice",7,"purple");
+        Frog frog2 = new Frog("Katherine",5,"Red");
+    }
+}
