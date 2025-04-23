11c91732f1d69dcb5642f024fa68b2b564675958
macman1717 <mchawk1717@gmail.com>
2025-04-23 10:11:31 -0400

    week 13 code

---
+++ Week_13_Final_Review/CollegeStudentTester.java
@@ -0,0 +1,27 @@
+package Week_13_Final_Review;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CollegeStudentTester {
+    public static void main(String[] args) {
+        //Read students.txt into array list of strings
+
+
+        //use the array list of strings to create an array list of CollegeStudent objects
+
+
+        //Create an InvalidStudentException
+        //Modify the CollegeStudent class, throwing exceptions whenever an invalid action is attempted
+        //(setting age to a negative, setting 900 number to a value greater than or less than 9 numbers, etc.)
+
+
+        //implement cloneable and use clone to create and add another student to the list
+
+
+        //implement comparable and sort the list
+
+
+    }
+
+}
---
+++ Week_13_Final_Review/RecursionTester.java
@@ -0,0 +1,17 @@
+package Week_13_Final_Review;
+
+public class RecursionTester {
+    public static void main(String[] args) {
+        //Write a recursive method to find the sum of the digits of a given integer
+        //example: 842 -> 14
+
+
+        //Write a recursive method that returns the given string reversed
+        //example: "Connor Griffin" -> "niffirG ronnoC"
+
+
+        //Given a string, compute recursively (no loops) the number of "11" substrings in the string.
+        //The "11" substrings should not overlap
+        //example: "word 1117 11doodle11 1111" -> 5
+    }
+}
