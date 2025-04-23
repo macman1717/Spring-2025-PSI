55c455f269596e3cda13fd1ac858114fe7f9ed4a
macman1717 <mchawk1717@gmail.com>
2025-04-23 15:46:39 -0400

    hello

--- Week_13_Final_Review/CollegeStudentTester.java
+++ Week_13_Final_Review/CollegeStudentTester.java
@@ -1,14 +1,43 @@
 package Week_13_Final_Review;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class CollegeStudentTester {
     public static void main(String[] args) {
         //Read students.txt into array list of strings
+        ArrayList<String> students = new ArrayList<String>();
+        try(BufferedReader br = new BufferedReader(new  FileReader("Week_13_Final_Review/students.txt"))){
+            String currentLine;
+            while((currentLine = br.readLine()) != null){
+                students.add(currentLine);
+            }
+        }catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
 
+        for(String student : students){
+            System.out.println(student);
+        }
 
         //use the array list of strings to create an array list of CollegeStudent objects
+        ArrayList<CollegeStudent> collegeStudents = new ArrayList<>();
+        for(String student : students){
+            String[] studentArray = student.split(",");
+            int studentID = Integer.parseInt(studentArray[1].trim());
+            int age = Integer.parseInt(studentArray[2].trim());
+            String name = studentArray[0].trim();
+            String major = studentArray[3].trim();
+            CollegeStudent collegeStudent = new CollegeStudent(studentID, name, major, age);
+            collegeStudents.add(collegeStudent);
+        }
+        for(CollegeStudent collegeStudent : collegeStudents){
+            System.out.println(collegeStudent);
+        }
 
 
         //Create an InvalidStudentException
